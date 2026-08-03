b587ba932adcc1f0055195cf738dd47697ee3684
Juxo28 <julianpalaciol2804@gmail.com>
2026-08-03 17:44:20 -0500

    Creacion del index.php

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../class/Gasto.php";
+
+$usuario1 = new Gasto(
+    2,
+    100000,
+    "03/08/2026",
+    "Se hizo una transacción para comprar implementos de oficina",
+    "Nequi",
+    "comprobante_compra_oficina_03082026.pdf",
+    "Caldas",
+    "Miscelania",
+    "Julian Palacio"
+);
+
+$usuario1->setId(1);
+$usuario1->setMetodoPago("Bancolombia");
+$usuario1->setUsuarioGasto("David Latorre");
+
+
+
+$usuario1->mostrarInformacion();
+
+?>
